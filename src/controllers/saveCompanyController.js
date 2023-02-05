d4e4e20a114af446b6dcde5c5060d6ca0de8d99b
Harbir Singh <harbir066@gmail.com>
2023-02-05 19:27:37 +0530

    test : update test cases for saveComapanyController

--- src/controllers/saveCompanyController.js
+++ src/controllers/saveCompanyController.js
@@ -1,13 +1,20 @@
+const { HTTPError } = require('../../errors/customError');
 const saveServices = require('../services/saveCompanyServices');
 const getCompaniesData = async (req, res) => {
   try {
     const { urlLink } = req.body;
-    const data = await saveServices.getCompaniesData(urlLink);
-    res
-      .status(201)
-      .json({ message: `${data} companies created in the database` });
+    const length = await saveServices.saveCompaniesData(urlLink);
+    const data = await saveServices.getCompaniesData();
+    res.status(201).json({
+      message: `Total off ${length} companies created`,
+      data: data,
+    });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    if (err instanceof HTTPError) {
+      res.status(err.statusCode).json({ message: err.message });
+    } else {
+      res.status(500).json({ message: err.message });
+    }
   }
 };
 
--- tests/controllers/saveCompanyContoller.test.js
+++ tests/controllers/saveCompanyContoller.test.js
@@ -1,10 +1,15 @@
 const saveServices = require('../../src/services/saveCompanyServices');
 const saveController = require('../../src/controllers/saveCompanyController');
+const { HTTPError } = require('../../errors/customError');
 
 describe('Testing Save Company Controller', () => {
   describe('Testing getCompaniesData function', () => {
     it('should return number of companies when input req with url in body is given', async () => {
-      jest.spyOn(saveServices, 'getCompaniesData').mockResolvedValue(1);
+      jest.spyOn(saveServices, 'saveCompaniesData').mockResolvedValue(1);
+      jest.spyOn(saveServices, 'getCompaniesData').mockResolvedValue({
+        id: 1,
+        name: 'name',
+      });
       const mockReq = {
         body: {
           urlLink: 'urlLink',
@@ -17,11 +22,30 @@ describe('Testing Save Company Controller', () => {
       await saveController.getCompaniesData(mockReq, mockRes);
       expect(mockRes.status).toHaveBeenCalledWith(201);
       expect(mockRes.json).toHaveBeenCalledWith({
-        message: '1 companies created in the database',
+        message: 'Total off 1 companies created',
+        data: { id: 1, name: 'name' },
       });
     });
-    it('should 500 status code error if services give error', async () => {
+    it('should throw HTTPError when no data is reterieved by the function', async () => {
+      const err = new HTTPError(404, 'No Companies Data Found');
+      jest.spyOn(saveServices, 'saveCompaniesData').mockResolvedValue(1);
+      jest.spyOn(saveServices, 'getCompaniesData').mockRejectedValue(err);
+      const mockReq = {
+        body: {
+          urlLink: 'urlLink',
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      await saveController.getCompaniesData(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(404);
+      expect(mockRes.json).toHaveBeenCalledWith({ message: err.message });
+    });
+    it('should throw 500 status code error if services give error', async () => {
       const err = new Error('Internal Server Error');
+      jest.spyOn(saveServices, 'saveCompaniesData').mockResolvedValue(1);
       jest.spyOn(saveServices, 'getCompaniesData').mockRejectedValue(err);
       const mockReq = {
         body: {
@@ -34,7 +58,7 @@ describe('Testing Save Company Controller', () => {
       };
       await saveController.getCompaniesData(mockReq, mockRes);
       expect(mockRes.status).toHaveBeenCalledWith(500);
-      expect(mockRes.json).toHaveBeenCalledWith({ error: err.message });
+      expect(mockRes.json).toHaveBeenCalledWith({ message: err.message });
     });
   });
 });
